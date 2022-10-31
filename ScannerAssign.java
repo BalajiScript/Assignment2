8b76685e534922d09cef4b8e8f02dadbf9b5c69d
BalajiScript <115561823+BalajiScript@users.noreply.github.com>
2022-10-31 21:37:36 +0800

    Update ScannerAssign.java

--- ScannerAssign.java
+++ ScannerAssign.java
@@ -1,47 +1,58 @@
-package startedLearningJava;
+package testHomeWork;
 
 import java.util.Scanner;
 
-public class ScannerAssign {
-	
-	String name;
-	String email;
-	int phoneNum;
-	String address;
-	String Status;
-//I'm not able to solve this assignment, can you pls give me to correct code for this assignment pls.
-//send me in email if possible	
+public class ScannerClassHW {
+
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
 		
-		//int arr[] =new int[3];
-		String[] arr = new String[3];
-        for (int i = 0; i < arr.length; i++) {
-        	
-        	
-            System.out.println("Please enter name");
-           
-   		arr[i] = sc.next();
-        // String a1=sc.next(); 
-   	
-   		for(int j : i) {
-           		System.out.println("Please enter email");
-            
-            		arr[j] = sc.next();
+		   Scanner wk = new Scanner(System.in);
+		
+			
+			System.out.println("Please enter number of student");
+			int count = wk.nextInt();
 
+			String[] name = new String[count];
+			String[] email = new String[count];
+
+			for (int i = 0; i < count; i++) {
+			
+			
+			System.out.println("Please enter name");
+
+			name[i] = wk.next();
+
+			System.out.println("Please enter email");
+
+			email[i] = wk.next();
+			
 		}
-   		}
-  		
-		System.out.println("Please enter which student details are you looking for");
-		int a5 = sc.nextInt();
-   		System.out.println(a5);
+			
+			System.out.println("Please enter which student details are you looking for");
+			int b = wk.nextInt();
 
-		
-	
-		System.out.println(arr[a5]);
-		System.out.println(arr[a5]);
+			System.out.println(name[b]);
+			System.out.println(email[b]);
 
 	}
 
 }
+**************************output***************************************************
+Please enter number of student
+3
+Please enter name
+Mukesh
+Please enter email
+Mukesh@gmail.com
+Please enter name
+Hitesh
+Please enter email
+Hitesh@gmail.com
+Please enter name
+Saksham
+Please enter email
+saksham@gmail.com
+Please enter which student details are you looking for
+1
+Hitesh
+Hitesh@gmail.com
